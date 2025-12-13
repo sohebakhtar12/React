e2ae4f871ed2345bf0ad13a63338fffb8193cad6
Soheb Akhtar <126268043+sohebakhtar12@users.noreply.github.com>
2025-12-13 11:28:10 +0530

    02_counter_codes

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+import Chai from "./chai";
+
+function App() {
+    return (
+
+        <
+        >
+        <
+        Chai / >
+        <
+        h1 > chai aur react < /h1> <
+        />
+    );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+  
+
+  return (
+    <>
+    <p1>Hii Soheb </p1>
+    <Chai/>
+    <h3>come fast</h3>
+    </> 
+  )
+}
+
+export default App
